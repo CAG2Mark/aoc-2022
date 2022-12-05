72e1a9ee592a6c1df79fc85611c1a97e56e19ea7
CAG2Mark <markng989@gmail.com>
2022-12-05 13:25:15 +0800

    day 5

---
+++ day5/p1.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include "../utils/aocutils.h"
+
+using namespace std;
+
+void solve() {
+    vector<string> lns = get_lines();
+
+    vector<deque<char>> stacks(9);
+
+    int i = 0;
+    for (; i < lns.size(); ++i) {
+        string ln = lns[i];
+        if (string_is_whitespace(ln)) break;
+
+        if (ln[1] == '1') break;
+
+        for (int i = 0; i < 9; ++i) {
+            int pos = 1 + 4*i;
+            if (ln[pos] == ' ') continue;
+            stacks[i].push_front(ln[pos]);
+        }
+    }
+
+    i += 2;
+    for (; i < lns.size(); ++i) {
+        vector<string> spl = split_str(lns[i], " ");
+
+        int amount = stoi(spl[1]);
+        int from = stoi(spl[3]) - 1;
+        int to = stoi(spl[5]) - 1;
+
+        for (int i = 0; i < amount; ++i) {
+            char mv = stacks[from].back();
+            stacks[to].push_back(mv);
+            stacks[from].pop_back();
+        }
+    }
+
+    for (int i = 0; i < 9; ++i) {
+        cout << stacks[i].back();
+    }
+    cout << "\n";
+}
+
+int main() {
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+
+    solve();
+}
