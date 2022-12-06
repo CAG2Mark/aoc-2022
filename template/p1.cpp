2f854962c259232001f0fe71fac14b7419313c81
CAG2Mark <markng989@gmail.com>
2022-12-06 14:11:54 +0800

    new template

--- template/p1.cpp
+++ template/p1.cpp
@@ -3,13 +3,19 @@
 
 using namespace std;
 
-void solve() {
-    vector<string> lns = get_lines();
+void solve(string filename) {
+    vector<string> lns = get_lines(filename);
 }
 
 int main() {
     // ios_base::sync_with_stdio(false);
     // cin.tie(NULL);
 
-    solve();
+    if (ifstream("input.test")) {
+        cout << "TEST INPUT OUTPUT\n---------------------\n";
+        solve("input.test");
+        cout << "\n";
+    }
+    cout << "OUTPUT\n---------------------\n";
+    solve("input");
 }
